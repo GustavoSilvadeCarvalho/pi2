5ab0c43b93bf583955f9c2a13ae98d29307f00b1
Gustavo Silva <gustacar2008@gmail.com>
2024-11-22 15:33:00 -0300

    corrigindo erros para o vercel

--- src/modules/auth/services/auth-services.ts
+++ src/modules/auth/services/auth-services.ts
@@ -40,6 +40,8 @@ async function isSessionValid() {
 }
 
 function destroySessions() {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
     cookies().delete('session');
 }
 
